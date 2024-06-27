017f8bf4b00d742e61406f05e277adc99de44731
vedant2709 <vedantchaudhary2002@gmail.com>
2024-06-27 15:37:10 +0530

    27 June - Dedugger added, process.env.NODE_ENV=development, config file setup for mongoose connection, owner model routes for creating owner and also some validations added to it.

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,28 @@
-const express=require("express");
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
+const ownerModel = require("../models/owner-model");
 
-router.get("/",function(req,res){
-    res.send("hey it's working from owners route");
-})
+if (process.env.NODE_ENV === "development") {
+  router.post("/create", async function (req, res) {
+    let owners = await ownerModel.find();
+    if (owners.length > 0) {
+      return res
+        .status(503)
+        .send("You don't have permission to create a new owner");
+    }
 
-module.exports=router;
+    let { fullname, email, password } = req.body;
+    let createdOwner=await ownerModel.create({
+      fullname,
+      email,
+      password,
+    });
+    res.status(201).send(createdOwner);
+  });
+}
+
+router.get("/", function (req, res) {
+  res.send("hey it's working from owners route");
+});
+
+module.exports = router;
