eb3af54c81b6b0fa0fb5f11489b1f558983c466d
vedant2709 <vedantchaudhary2002@gmail.com>
2024-06-27 17:49:26 +0530

    June 27 - index.ejs file is created and loging register form is created in it

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path=require("path");
 const ownersRouter=require("./routes/ownersRouter")
 const productsRouter=require("./routes/productsRouter")
 const usersRouter=require("./routes/usersRouter")
+const index=require("./routes/index")
 
 const db=require("./config/mongoose-connection");
 
@@ -17,6 +18,7 @@ app.set("view engine","ejs");
 app.use("/owners",ownersRouter);
 app.use("/users",usersRouter);
 app.use("/products",productsRouter);
+app.use("/",index)
 
 app.listen(3000,()=>{
     console.log("Listening on 3000");
