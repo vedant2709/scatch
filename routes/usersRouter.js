8dc9db3bdecd8b4c7465ccf11d7b48cf555c0836
vedant2709 <vedantchaudhary2002@gmail.com>
2024-06-27 18:40:50 +0530

    commit

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,49 @@
-const express=require("express");
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const userModel = require("../models/user-model");
 
-router.get("/",function(req,res){
-    res.send("hey it's working from users route");
-})
+router.get("/", function (req, res) {
+  res.send("hey it's working from users route");
+});
 
-module.exports=router;
+router.post(
+  "/register",
+  [
+    body("email")
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Enter a valid email address"),
+    body("password")
+      .notEmpty()
+      .withMessage("Password is required")
+      .isLength({ min: 3 })
+      .withMessage("Password must be at least 3 characters long"),
+    body("fullname")
+      .notEmpty()
+      .withMessage("Full name is required")
+      .isLength({ min: 1 })
+      .withMessage("Full name cannot be empty"),
+  ],
+  async function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      let { email, password, fullname } = req.body;
+      let user = await userModel.create({
+        email,
+        password,
+        fullname,
+      });
+      res.send(user);
+    } catch (error) {
+      res.send(error.message);
+    }
+  }
+);
+
+module.exports = router;
